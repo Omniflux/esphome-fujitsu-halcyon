Omniflux/esphome-fujitsu-halcyon

commit 6fa2def167ca1e22eec9edb41da9ff7174ac6d9c

Drop usage of UART event queue

components/fujitsu-halcyon/esphome-fujitsu-halcyon.cpp
@@ -12,7 +12,19 @@ static const auto TAG = "esphome::fujitsu_general_airstage_h_controller";
 
 constexpr std::array ControllerName = { "Primary", "Secondary", "Undocumented" };
 
+void FujitsuHalcyonController::loop() {
+    this->controller->process_uart_data();
+}
+
 void FujitsuHalcyonController::setup() {
+    // Currently no way to do this in IDFUARTComponent YAML configuration without setting the flow control pin.
+    // Using RTS is not needed, but the side effect of suppressing input during output is, as the LIN chip provides loopback.
+    if (auto err = uart_set_mode(static_cast<uart_port_t>(static_cast<uart::IDFUARTComponent*>(this->parent_)->get_hw_serial_number()), UART_MODE_RS485_HALF_DUPLEX) != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to set UART mode: %s", esp_err_to_name(err));
+        this->mark_failed();
+        return;
+    }
+
     this->controller = new fujitsu_general::airstage::h::Controller(
         static_cast<uart::IDFUARTComponent*>(this->parent_)->get_hw_serial_number(),
         this->controller_address_,
@@ -35,15 +47,8 @@ void FujitsuHalcyonController::setup() {
                 this->write_array(buf, length);
                 this->log_buffer("TX", buf, length);
             }
-        },
-        *static_cast<uart::IDFUARTComponent*>(this->parent_)->get_uart_event_queue()
+        }
     );
-
-    if (!this->controller->start()) {
-        ESP_LOGE(TAG, "Failed to start controller");
-        this->mark_failed();
-        return;
-    }
 
     this->connected_sensor->publish_state(false);
 

components/fujitsu-halcyon/Controller.cpp
@@ -12,125 +12,23 @@ namespace fujitsu_general::airstage::h {
 
 static const char* TAG = "fujitsu_general::airstage::h::Controller";
 
-bool Controller::start() {
-    int err;
-    auto uart_config = UARTConfig;
-    constexpr int intr_alloc_flags = 0;
-    constexpr uint32_t queue_size = 20; // ?
-    constexpr uint32_t stack_depth = 4096; // ?
-    constexpr UBaseType_t task_priority = 12; // ?
+void Controller::process_uart_data() {
+    auto buffer_len = this->uart_available_bytes();
+    if (buffer_len >= Packet::FrameSize) {
+        Packet::Buffer buffer;
 
-    // User should have called uart_set_pin before this point if necessary
-
-    if (this->uart_event_queue == nullptr && uart_is_driver_installed(this->uart_num)) {
-        err = uart_driver_delete(this->uart_num);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "Failed to delete UART driver: %s", esp_err_to_name(err));
-            return false;
+        // Discard partial frame
+        if (auto discard = buffer_len % buffer.size()) {
+            this->uart_read_bytes(buffer.data(), discard);
+            ESP_LOGW(TAG, "Discarded %d bytes", discard);
         }
-    }
 
-    if (!uart_is_driver_installed(this->uart_num)) {
-        const auto buffer_size = UART_HW_FIFO_LEN(this->uart_num) * 2;
-        err = uart_driver_install(this->uart_num, buffer_size, buffer_size, queue_size, &this->uart_event_queue, intr_alloc_flags);
-        if (err != ESP_OK) {
-            ESP_LOGE(TAG, "Failed to install UART driver: %s", esp_err_to_name(err));
-            return false;
+        // For each frame
+        while (buffer_len) {
+            this->uart_read_bytes(buffer.data(), buffer.size());
+            buffer_len = this->uart_available_bytes();
+            this->process_packet(buffer, buffer_len == 0 /* Indicates final packet on wire */);
         }
-    }
-
-    err = uart_param_config(this->uart_num, &uart_config);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to configure UART: %s", esp_err_to_name(err));
-        return false;
-    }
-
-    err = uart_set_mode(this->uart_num, UART_MODE_RS485_HALF_DUPLEX);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to set UART mode: %s", esp_err_to_name(err));
-        return false;
-    }
-
-    // Ensure large enough not to trigger mid frame, resynchronize from rx_timeout instead
-    err = uart_set_rx_full_threshold(this->uart_num, Packet::FrameSize * 4);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to set UART RX full threshold: %s", esp_err_to_name(err));
-        return false;
-    }
-
-    // Default timeout (time to transmit 10 characters at 500bps) is too long
-    // If we wait for default timeout the transmit window is over before processing even begins.
-    err = uart_set_rx_timeout(this->uart_num, UARTInterPacketSymbolSpacing);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to set UART RX timeout: %s", esp_err_to_name(err));
-        return false;
-    }
-
-    xTaskCreate([](void* o){ static_cast<Controller*>(o)->uart_event_task(); }, "UART_Event", stack_depth, this, task_priority, NULL);
-
-    return true;
-}
-
-void Controller::uart_event_task() {
-    uart_event_t event;
-
-    for(;;) {
-        if (xQueueReceive(this->uart_event_queue, &event, portMAX_DELAY)) {
-            switch(event.type) {
-                [[likely]] case UART_DATA:
-                    Packet::Buffer buffer;
-                    size_t buffer_len;
-
-                    // Discard partial frame
-                    buffer_len = this->uart_available_bytes();
-                    if (auto discard = buffer_len % buffer.size()) {
-                        this->uart_read_bytes(buffer.data(), discard);
-                        ESP_LOGW(TAG, "Discarded %d bytes", discard);
-                    }
-
-                    // For each frame
-                    for (auto i = 0; i < event.size / buffer.size(); i++) {
-                        this->uart_read_bytes(buffer.data(), buffer.size());
-                        buffer_len = this->uart_available_bytes();
-                        this->process_packet(buffer, buffer_len == 0 /* Indicates final packet on wire */);
-                    }
-
-                    break;
-
-                case UART_BREAK:
-                    // TODO Why rx break after tx?
-                    ESP_LOGD(TAG, "UART break!");
-                    break;
-
-                case UART_BUFFER_FULL:
-                    // Something went wrong - don't try to catch up,
-                    // just discard all pending data and start over
-                    ESP_LOGW(TAG, "UART ring buffer full!");
-                    uart_flush_input(this->uart_num);
-                    xQueueReset(this->uart_event_queue);
-                    break;
-
-                case UART_FIFO_OVF:
-                    // Something went wrong - don't try to catch up,
-                    // just discard all pending data and start over
-                    ESP_LOGW(TAG, "UART FIFO Overflow!");
-                    uart_flush_input(this->uart_num);
-                    xQueueReset(this->uart_event_queue);
-                    break;
-
-                case UART_PARITY_ERR:
-                    ESP_LOGW(TAG, "UART parity error");
-                    break;
-
-                case UART_FRAME_ERR:
-                    ESP_LOGW(TAG, "UART frame error");
-                    break;
-
-                default:
-                    ESP_LOGW(TAG, "Unhandled UART event type: %d", event.type);
-                    break;
-            }
-        }   
     }
 }
 

components/fujitsu-halcyon/Controller.h
@@ -4,8 +4,6 @@
 #include <functional>
 #include <queue>
 
-#include <freertos/FreeRTOS.h>
-#include <freertos/queue.h>
 #include <driver/uart.h>
 
 #include "Packet.h"
@@ -103,12 +101,12 @@ class Controller {
     };
 
     public:
-        Controller(uint8_t uart_num, uint8_t controller_address, const Callbacks& callbacks, QueueHandle_t uart_event_queue = nullptr)
-            : uart_num(static_cast<uart_port_t>(uart_num)), controller_address(controller_address), uart_event_queue(uart_event_queue), callbacks(callbacks) {
+        Controller(uint8_t uart_num, uint8_t controller_address, const Callbacks& callbacks)
+            : uart_num(static_cast<uart_port_t>(uart_num)), controller_address(controller_address), callbacks(callbacks) {
             this->set_initialization_stage(InitializationStageEnum::DetectFeatureSupport);
         }
 
-        bool start();
+        void process_uart_data();
         bool is_initialized() const { return this->initialization_stage == InitializationStageEnum::Complete; }
         void reinitialize() { this->set_initialization_stage(InitializationStageEnum::DetectFeatureSupport); }
         InitializationStageEnum get_initialization_stage() const { return this->initialization_stage; }
@@ -143,7 +141,6 @@ class Controller {
     private:
         uart_port_t uart_num;
         uint8_t controller_address;
-        QueueHandle_t uart_event_queue;
         Callbacks callbacks;
 
         struct Features features = {};
@@ -153,7 +150,6 @@ class Controller {
         std::queue<struct Function> function_queue;
         bool last_error_flag = false; // TODO handle errors for multiple indoor units...multiple errors per IU?
 
-        [[noreturn]] void uart_event_task();
         size_t uart_available_bytes();
         void uart_read_bytes(uint8_t *buf, size_t length);
         void uart_write_bytes(const uint8_t *buf, size_t length);

components/fujitsu-halcyon/climate.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+import esphome.final_validate as fv
 
 from esphome.components import (
     binary_sensor,
@@ -26,13 +27,14 @@ from esphome.const import (
     CONF_INTERNAL,
     CONF_MODE,
     CONF_NAME,
+    CONF_UART_ID,
     DEVICE_CLASS_TEMPERATURE,
     DEVICE_CLASS_CONNECTIVITY,
     DEVICE_CLASS_PROBLEM,
     ENTITY_CATEGORY_CONFIG,
     ENTITY_CATEGORY_DIAGNOSTIC,
     STATE_CLASS_MEASUREMENT,
-    UNIT_CELSIUS
+    UNIT_CELSIUS,
 )
 
 from esphome.types import ConfigType
@@ -82,6 +84,11 @@ CustomNumber = custom_ns.class_("CustomNumber", cg.Component, number.Number)
 CustomSwitch = custom_ns.class_("CustomSwitch", cg.Component, switch.Switch)
 fujitsu_general_airstage_h_controller_ns = cg.esphome_ns.namespace("fujitsu_general_airstage_h_controller")
 FujitsuHalcyonController = fujitsu_general_airstage_h_controller_ns.class_("FujitsuHalcyonController", cg.Component, climate.Climate, uart.UARTDevice)
+
+PACKET_FRAME_SIZE = 8
+UART_INTER_PACKET_SYMBOL_SPACING = 2
+
+COMPONENT_NAME = __name__.split('.')[-2]
 
 CONFIG_SCHEMA = climate.climate_schema(FujitsuHalcyonController).extend(
     {
@@ -169,14 +176,49 @@ CONFIG_SCHEMA = climate.climate_schema(FujitsuHalcyonController).extend(
 if TZSP_AVAILABLE:
     CONFIG_SCHEMA = CONFIG_SCHEMA.extend(tzsp.TZSP_SENDER_SCHEMA)
 
-FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
-    "fujitsu_halcyon",
-    require_tx=True,
-    require_rx=True,
-    baud_rate=500,
-    data_bits=8,
-    parity="EVEN",
-    stop_bits=1
+def final_validate_uart_schema(config):
+    def validate_rx_full_threshold(value):
+        if not isinstance(value, int) or value < PACKET_FRAME_SIZE * 2:
+            raise cv.Invalid(f"Component {COMPONENT_NAME} requires {uart.CONF_RX_FULL_THRESHOLD} >= {PACKET_FRAME_SIZE * 2}  for the uart referenced by {CONF_UART_ID}")
+        return value
+
+    def validate_rx_timeout(value):
+        if value != UART_INTER_PACKET_SYMBOL_SPACING:
+            raise cv.Invalid(f"Component {COMPONENT_NAME} requires {uart.CONF_RX_TIMEOUT} = {UART_INTER_PACKET_SYMBOL_SPACING} for the uart referenced by {CONF_UART_ID}")
+        return value
+
+    # This should not be done this way; Not sure of the proper way to do it...
+    full_config = fv.full_config.get()
+    uart_path = full_config.get_path_for_id(config[CONF_UART_ID])[:-1]
+    uart_conf = full_config.get_config_for_path(uart_path)
+    if uart.CONF_RX_FULL_THRESHOLD not in uart_conf:
+        uart_conf[uart.CONF_RX_FULL_THRESHOLD] = PACKET_FRAME_SIZE * 2
+
+    cv.Schema(
+        {
+            cv.Required(CONF_UART_ID): fv.id_declaration_match_schema(
+                {
+                    cv.Optional(uart.CONF_RX_FULL_THRESHOLD, default=PACKET_FRAME_SIZE * 2): validate_rx_full_threshold,
+                    cv.Optional(uart.CONF_RX_TIMEOUT, default=UART_INTER_PACKET_SYMBOL_SPACING): validate_rx_timeout,
+                },
+            ),
+        },
+        extra=cv.ALLOW_EXTRA,
+    )(config)
+
+    return config
+
+FINAL_VALIDATE_SCHEMA = cv.All(
+    final_validate_uart_schema,
+    uart.final_validate_device_schema(
+        COMPONENT_NAME,
+        require_tx=True,
+        require_rx=True,
+        baud_rate=500,
+        data_bits=8,
+        parity="EVEN",
+        stop_bits=1,
+    ),
 )
 
 async def to_code(config: ConfigType) -> None:

components/fujitsu-halcyon/esphome-fujitsu-halcyon.h
@@ -57,6 +57,7 @@ class FujitsuHalcyonController : public Component, public climate::Climate, publ
 
         FujitsuHalcyonController(uart::IDFUARTComponent *parent, uint8_t controller_address) : uart::UARTDevice(parent), controller_address_(controller_address) {}
 
+        void loop() override;
         void setup() override;
         void dump_config() override;
         float get_setup_priority() const override { return esphome::setup_priority::DATA; }

commit cc40f9e98adcc2b647588fb0cb83e35f15268694

Fix operator precedence error in uart setup error check introduced in 6fa2def

components/fujitsu-halcyon/esphome-fujitsu-halcyon.cpp
@@ -18,7 +18,7 @@ void FujitsuHalcyonController::loop() {
 void FujitsuHalcyonController::setup() {
     // Currently no way to do this in IDFUARTComponent YAML configuration without setting the flow control pin.
     // Using RTS is not needed, but the side effect of suppressing input during output is, as the LIN chip provides loopback.
-    if (auto err = uart_set_mode(static_cast<uart_port_t>(static_cast<uart::IDFUARTComponent*>(this->parent_)->get_hw_serial_number()), UART_MODE_RS485_HALF_DUPLEX) != ESP_OK) {
+    if (auto err = uart_set_mode(static_cast<uart_port_t>(static_cast<uart::IDFUARTComponent*>(this->parent_)->get_hw_serial_number()), UART_MODE_RS485_HALF_DUPLEX); err != ESP_OK) {
         ESP_LOGE(TAG, "Failed to set UART mode: %s", esp_err_to_name(err));
         this->mark_failed();
         return;